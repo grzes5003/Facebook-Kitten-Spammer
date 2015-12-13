63821892ce3fdf70907917dab6464766eac557b7
Grzegorz <grzes.kulinski@gmail.com>
2015-12-13 23:04:46 +0100

    Update xd.js

--- xd.js
+++ xd.js
@@ -2,9 +2,12 @@ var chatBox  = document.getElementsByName("message_body")[0];
 var replyBut = document.getElementById("u_0_t");
  
 var kitInt   = setInterval(function() {
-    chatBox.classList.remove("DOMControl_placeholder")
-    chatBox.value = new Date().getHours() + ":"+ new Date().getMinutes();
- 
+	//if(Date().getHours() == Date().getMinutes())
+	var time = new Date().getHours();
+	if(time == 23){
+    		chatBox.classList.remove("DOMControl_placeholder")
+    		chatBox.value = new Date().getHours() + ":"+ new Date().getMinutes();
+	}
 }, 6000);
 
 var replyClicker = setInterval(function(){
